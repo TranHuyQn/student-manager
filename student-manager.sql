7e416d524944c5106eb5cb2c34f227f228d79a23
tranhuy <tranvanhuy0202@gmail.com>
2019-08-14 09:08:38 +0700

    add new file sql

--- student-manager.sql
+++ student-manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 13, 2019 at 11:55 AM
+-- Generation Time: Aug 14, 2019 at 09:07 AM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.1
 
@@ -28,16 +28,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `students` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `name`) VALUES
-(1, 'Trần văn huy'),
-(2, 'Son Hai');
+INSERT INTO `students` (`id`, `name`, `email`) VALUES
+(40, 'trần văn huy', 'tranvanhuy0202@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -47,7 +47,8 @@ INSERT INTO `students` (`id`, `name`) VALUES
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -57,7 +58,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
